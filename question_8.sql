af2fcea6b48b515323e6c9822ddf763e6246f193
Isaac Martins <91567120+IsaacMartins12@users.noreply.github.com>
2023-01-15 19:57:45 -0400

    Update question_8.sql

--- question_8.sql
+++ question_8.sql
@@ -1,27 +1,27 @@
-CREATE TABLE IF NOT EXISTS "dogs" (  -- Criação da tabela "dogs"
+CREATE TABLE IF NOT EXISTS "dogs" (  -- Creating the "dogs" table
 	id INTEGER NOT NULL PRIMARY KEY,
     name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "cats" (  -- Criação da tabela "cats"
+CREATE TABLE IF NOT EXISTS "cats" (  -- Creating the "cats" table
 	id INTEGER NOT NULL PRIMARY KEY,
     name VARCHAR(50) NOT NULL
 );
 
--- Inserir alguns nomes na tabela "cats"
+-- Insert some names into the "cats" table
 
 INSERT INTO cats (name) VALUES ("Robert"); 
 INSERT INTO cats (name) VALUES ("Felix"); 
 INSERT INTO cats (name) VALUES ("Black"); 
 INSERT INTO cats (name) VALUES ("Rex");
 
--- Inserir alguns nomes na tabela "dogs"
+-- Insert some names into the "dogs" table
 
 INSERT INTO dogs (name) VALUES ("Lady");
 INSERT INTO dogs (name) VALUES ("Linda");
 INSERT INTO dogs (name) VALUES ("Black");
 INSERT INTO dogs (name) VALUES ("Rex");
 
--- Executar a consulta especificada na questao
+-- Run the query specified in the question
 
 SELECT DISTINCT name from dogs UNION SELECT DISTINCT name from cats
